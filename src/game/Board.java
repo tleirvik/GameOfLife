85fc2e089c3516f9aa26e7bdab76783bc7cda25c
FullRobotRulle <r.mini.me@gmail.com>
2016-01-22 10:05:00 +0100

    Fjernet GameLogic og la til GoL samt GoLClassic

--- src/game/Board.java
+++ src/game/Board.java
@@ -3,6 +3,8 @@
  */
 package game;
 
+import java.util.Random;
+
 /**
  * @author 
  *
@@ -12,7 +14,7 @@ public abstract class Board {
 	private int columns;
 	private int rows;
 	
-	public Cell[][] boardGrid = null;
+	public Cell[][] boardGrid;
 	
 	/*
 	
@@ -21,6 +23,23 @@ public abstract class Board {
 		boardGrid = new Cell[this.rows][this.columns];
 	}
 	*/
+	
+	//Burde egentlig returnere et array med boolean verdier (bit 0 1) like langt som antall celler i et brett
+	//Eksempel: 10x10 grid = 100 celler
+	static boolean seedGenerator(/*long seed*/) {
+		//La bruker lage sin egen seed
+		//Bruk datamaskinens klosse?
+		
+		Random rand = new Random();
+		
+		int randInt = rand.nextInt(2);
+		
+		if(randInt == 1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 	
 	
 	public abstract void populateBoard();
