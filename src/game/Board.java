425c2f3ec41e4aa9c12e484998345b2aca16a35d
stianreistadrogeberg <stianreistadrogeberg@dhcp-158-39-197-179.wlan.hioa.no>
2016-01-22 11:05:17 +0100

    Endret fredag 22/1 11:05

--- src/game/Board.java
+++ src/game/Board.java
@@ -4,33 +4,33 @@
 package game;
 
 import java.util.Random;
+import javafx.scene.paint.Color;
 
 public abstract class Board {
 	
 	
-	//Returnerer et et array med boolske verdier (bit 0 1) like langt som antall celler i et brett
-	//Eksempel: 10x10 grid = 100 celler
-	//Tidligere kalt en gang for hver celle via populateBoard()
-	boolean[] seedGenerator(int numberOfCells) {
-		
-		boolean[] seed = new boolean[numberOfCells];
-		
-		Random rand = new Random();
-		
-		for(int i = 0; i < seed.length; i++) {
-			if(rand.nextInt(2) == 1) {
-				seed[i] = true;
-			} else {
-				seed[i] = false;
-			}
-		}
-		
-		return seed;
-		
-	}
+    //Returnerer et et array med boolske verdier (bit 0 1) like langt som antall celler i et brett
+    //Eksempel: 10x10 grid = 100 celler
+    //Tidligere kalt en gang for hver celle via populateBoard()
+    boolean[][] seedGenerator(int rows, int columns) {
+
+            
+
+        Random rand = new Random();
+        boolean[][] seed = new boolean[rows][columns];
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                seed[i][j] = rand.nextBoolean();
+            }
+        }
+
+        return seed;
+
+    }
 	
-	public abstract void populateBoard();
-	public abstract int nearestNeighbour(Cell cell);
-	public abstract void nextGeneration();
+    public abstract void populateBoard();
+    public abstract int nearestNeighbour(Cell cell);
+    public abstract void nextGeneration();
 	
 }
