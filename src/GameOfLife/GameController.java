4c08bf39dc0ed9af5d99b9793893ad985549828e
stianreistadrogeberg <stianpiano@protonmail.com>
2016-02-09 12:55:07 +0100

    Kodet sammen tirsdag 9.februar

--- src/GameOfLife/GameController.java
+++ src/GameOfLife/GameController.java
@@ -1,6 +1,9 @@
 package GameOfLife;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -8,48 +11,70 @@ import javafx.stage.Stage;
  *
  *
  */
-public class GameController extends Application{
+public class GameController extends Application {
 
-	private GameOfLife gameOfLife;
+    
+	private GameOfLife gol;
 	private FileManagement fileManagement;
 
-	/**
-	 *
-	 */
-	public void play() {
-
-	}
+	
+        
 	/**
 	 *
 	 */
 	public void newGame() {
-
+            // hardkoder input (hest en dialogboks for input)
+            gol = new GameOfLife2D(!true, 0, 0);
 	}
+        
+        /**
+	 *
+	 */
+	public void play() {
+            if(gol.getIsBoardEmpty()) {
+                gol.populateBoard();
+            }
+	}
+        
 	/**
 	 *
 	 */
 	public void saveGame() {
 
 	}
+        
 	/**
 	 *
 	 */
 	public void loadGame() {
 
 	}
+        
 	/**
 	 *
 	 */
 	public void exportGame() {
 
 	}
+        
 	/**
 	 *
 	 */
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void start(Stage stage) throws Exception {
+            BorderPane root = (BorderPane) FXMLLoader.load(getClass().
+                getResource("MainWindow.fxml"));
+            
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("grafikk.css").
+                toExternalForm());
+            
+            stage.setTitle("Game Of Life");
+            stage.setScene(scene);
+            stage.show();
 	}
-
+        
+        public static void main(String[] args) {
+            launch(args);
+        }
 }
--- src/GameOfLife/GameOfLife.java
+++ src/GameOfLife/GameOfLife.java
@@ -1,15 +1,36 @@
 package GameOfLife;
-
+/**
+ * 
+ * @author stianreistadrogeberg
+ * 
+ * Implementer getter setter + variabel for om brettet er tomt (alle celler er false eller 0). 9/2/16
+ */
 public abstract class GameOfLife {
 
 	private int state;
 	private int currentState;
+        private boolean isBoardEmpty; // er false hvis alle celler på brettet er døde.
+        
+        public GameOfLife(int state, int curState) {
+            this.state = state;
+            currentState = curState;
+            isBoardEmpty = false; // strengt tatt ikke nødvendig (?)
+        }
+        
+        public void setIsBoardEmpty(boolean isEmpty) {
+            isBoardEmpty = isEmpty;
+        }
+        
+        public boolean getIsBoardEmpty() {
+            return isBoardEmpty;
+        }
 
 	public abstract void populateBoard();
 	public int seedGeneration() {
 
 		return 0;
 	}
+        
 	public abstract void nextGeneration();
 	public abstract int countNeighbours();
 }
