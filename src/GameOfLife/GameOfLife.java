b9668ac56f82af31171c548af190bfb36debb419
tleirvik <tleirvik>
2016-02-11 15:15:18 +0100

    Parprogrammering; endringer i GameOfLife-klassene

--- src/GameOfLife/GameOfLife.java
+++ src/GameOfLife/GameOfLife.java
@@ -1,41 +1,38 @@
 package GameOfLife;
+
+import java.util.Random;
+
 /**
- * 
+ *
  * @author stianreistadrogeberg
- * 
+ *
  * Implementer getter setter + variabel for om brettet er tomt (alle celler er false eller 0). 9/2/16
  */
 public abstract class GameOfLife {
 
-	private int state;
-	private int currentState;
     private boolean isBoardEmpty; // er true hvis alle celler på brettet er døde.
-        
-        /*public GameOfLife(int state, int curState) {
-            this.state = state;
-            currentState = curState;
-            isBoardEmpty = true; // strengt tatt ikke nødvendig (?)
-        }*/
-        
-        /*public void setIsBoardEmpty(boolean isEmpty) {
-            isBoardEmpty = isEmpty;
-        }*/
-        
-        /*public boolean getIsBoardEmpty() {
-            return isBoardEmpty;
-        }*/
 
     public abstract void setIsBoardEmpty(boolean boardEmpty);
     public abstract boolean getIsBoardEmpty();
-    
-	public abstract void populateBoard();
-	public int seedGeneration() {
-
-		return 0;
-	}
-        
+	public abstract void populateRandomBoard();
 	public abstract void nextGeneration();
 	public abstract int countNeighbours();
 	public abstract boolean[][] convertBoardToBoolean();
 
+
+	/**
+	 * The purpose of this method is to generate a pseudo-random boolean value.
+	 *
+	 * @param limit
+	 * @return boolean
+	 */
+	public boolean seedGeneration(int limit) {
+
+		Random random = new Random();
+
+		if (random.nextInt() != 1)
+			return false;
+		else
+			return true;
+	}
 }
--- src/GameOfLife/GameOfLife3D.java
+++ src/GameOfLife/GameOfLife3D.java
@@ -9,47 +9,6 @@ package GameOfLife;
  *
  * @author stianreistadrogeberg
  */
-public class GameOfLife3D extends GameOfLife {
+public class GameOfLife3D  {
 
-    public GameOfLife3D(int state, int curState) {
-        // super(state, curState);
-    }
-
-    @Override
-    public void populateBoard() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void nextGeneration() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int countNeighbours() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    /**
-     *  Too be implemented
-     */
-	@Override
-	public void setIsBoardEmpty(boolean boardEmpty) {
-		// TODO Auto-generated method stub
-	}
-	/**
-	 *  Too be implemented
-	 */
-	@Override
-	public boolean getIsBoardEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	/**
-	 *  Too be implemented
-	 */
-	@Override
-	public boolean[][] convertBoardToBoolean() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
