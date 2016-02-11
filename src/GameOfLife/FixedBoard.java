eb86983c337de62db0e82aa40b463bcdc928aa95
tleirvik <tleirvik>
2016-02-11 12:46:19 +0100

    Minor changes in FixedBoard and CellTest

--- src/GameOfLife/FixedBoard.java
+++ src/GameOfLife/FixedBoard.java
@@ -1,16 +1,22 @@
+/**
+ *
+ */
 package GameOfLife;
 
+
 /**
- * @author
+ * Concrete implementation of the abstract Board class.
+ * The purpose of this class is to offer an array which the cells reside in
  *
  */
 public class FixedBoard extends Board{
-    private Cell[][] cell;
-    
+
+	private Cell[][] cell;
+
     public FixedBoard(int rows, int columns) {
         super(rows, columns);
         cell = new Cell[rows][columns];
-        
+
         for(int row = 0; row < cell.length; row++) {
     		for(int col = 0; col < cell[row].length; col++) {
     			cell[row][col] = new Cell();
@@ -32,10 +38,10 @@ public class FixedBoard extends Board{
     public int getColumn() {
             return cell[0].length;
     }
-    
+
     /**
      * Returnerer cellen på gitt posisjon
-     * 
+     *
      * @param row
      * @param column
      * @return Cell-object
@@ -44,10 +50,15 @@ public class FixedBoard extends Board{
     public Cell getCell(int row, int column) {
     	return cell[row][column];
     }
-    
+	/**
+	 * The purpose of this method is to set the setIsAalive value of the entire grid
+	 *
+	 * @param boolean[][] grid
+	 *
+	 */
     //Tidligere setBoard-metoden
     public void setBoard(boolean[][] grid) {
-    	
+
     	for(int row = 0; row < grid.length; row++) {
     		for(int col = 0; col < grid[row].length; col++) {
     			System.out.println(cell[row][col]);
@@ -55,9 +66,11 @@ public class FixedBoard extends Board{
                 cell[row][col].setIsAlive( grid[row][col] );
     		}
     	}
-            
     }
-    
+    /**
+     *  This' class toString()-method
+     *  @return String The size of the board(x,y aka rows and columns)
+     */
     @Override
     public String toString() {
             return "A";
