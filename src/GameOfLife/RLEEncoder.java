e0285f8b4c7e7bd7939f38385c6fc1930ebc3080
tleirvik <tleirvik>
2016-02-19 14:41:33 +0100

    RLE

---
+++ src/GameOfLife/RLEEncoder.java
@@ -0,0 +1,16 @@
+package GameOfLife;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RLEEncoder {
+
+	private boolean[][] board;
+
+
+	public RLEEncoder(File file) throws IOException {
+
+
+	}
+
+}
