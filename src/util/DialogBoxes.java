b66405991e2ffcebf1c4ceb83924ed1f2eb394e0
tleirvik <tleirvik>
2016-03-15 14:48:32 +0100

    Fjernet dialogbokser, endringer i view'et

--- src/util/DialogBoxes.java
+++ src/util/DialogBoxes.java
@@ -31,7 +31,14 @@ import javafx.util.Pair;
 
 public class DialogBoxes {
 
-	public void infoBox(String title, String headerText, String contentText) {
+	/**
+     * Static function with the purpose of "throwing" dialog boxes
+     * @param title The title of the dialog box
+     * @param headerText The header text of the dialog box
+     * @param contentText The content text of the dialog box
+     */
+
+    public static void infoBox(String title, String headerText, String contentText) {
     	Alert alert = new Alert(AlertType.WARNING);
     	alert.setTitle(title);
     	alert.setHeaderText(headerText);
