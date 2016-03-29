b5fbb9d6a33522af15b83645f7745b28819cb9b1
tleirvik <tleirvik>
2016-03-29 12:08:53 +0200

    GameOfLife Unit Testing

--- src/GameOfLife/GameOfLife2DTest.java
+++ src/GameOfLife/GameOfLife2DTest.java
@@ -1,65 +1,67 @@
-//package GameOfLife;
-//
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//public class GameOfLife2DTest {
-//
-//	GameOfLife2D gol = new GameOfLife2D(false, 10, 10);
-//
-//	@Before
-//	public void setUp() throws Exception {
-//	}
-//
-//	@Test
-//	public void testGetCellAliveStatus() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetCellAliveStatus() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetIsBoardEmpty() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetIsBoardEmpty() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testPopulateRandomBoard() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testNextGeneration() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testCountNeighbours() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testConvertBoardToBoolean() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGameOfLife2DBooleanIntInt() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGameOfLife2DBooleanArrayArrayBoolean() {
-//		fail("Not yet implemented");
-//	}
-//
-//}
+package GameOfLife;
+import org.junit.Before;
+import org.testng.annotations.Test;
+
+import static org.junit.*;
+import static org.hamcrest.*;
+
+import static junit.framework.TestCase.fail;
+
+
+public class GameOfLife2DTest {
+
+	GameOfLife2D gol = new GameOfLife2D(false, 11, 11);
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testNextGeneration() {
+
+
+        byte[][] inputArraySmallExploder = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };
+
+        byte[][] inputArraySmallExploderAfter10Gen = {
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+            {0, 1, 1, 0 ,0 ,0 ,0 ,0, 1, 1, 0},
+            {1, 0, 0, 0, 0, 1, 0, 0, 0 ,0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 1, 0, 0, 0 ,0, 1},
+            {0, 1, 1, 0 ,0 ,0 ,0 ,0, 1, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+        };
+	}
+
+	@Test
+	public void testCountNeighbours() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGameOfLife2DBooleanIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGameOfLife2DBooleanArrayArrayBoolean() {
+		fail("Not yet implemented");
+	}
+
+}
