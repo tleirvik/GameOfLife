afaffcb6ab3ad280f09581f22f56a2dd837c90ec
FullRobotRulle <r.mini.me@gmail.com>
2016-03-29 10:28:40 +0200

    FixedBoard ferdig, deprecated

--- src/GameOfLife/FixedBoard.java
+++ src/GameOfLife/FixedBoard.java
@@ -1,68 +1,72 @@
-/**
- *
- */
 package GameOfLife;
 
-
 /**
- * Concrete implementation of the abstract Board class.
- * The purpose of this class is to offer an array which the cells reside in
- *
+ * @deprecated Deprecrated since 18.03.2016
+ * @author Robin Sean Aron David Lundh, Terje Leirvik, Stian Reistad Røgeberg.
+ * 
+ * This class contains the gameboard in a fixed size that cannot be changed
+ * after creation. It also contains the algorithm for Game Of Life specifically
+ * made for a board of a fixed size.
+ * 
  */
 public class FixedBoard {
-    private MetaData metadata;
-    private byte[][] currentCells;
-
+    private final MetaData metadata;
+    private final byte[][] currentGeneration;
+    private final byte[][] nextGeneration;
+    
     /**
+     * Constructs a board of a fixed size.
      *
-     * @param rows
-     * @param columns
+     * @param rows The number of rows
+     * @param columns The number of columns
      */
     public FixedBoard(int rows, int columns) {
         metadata = new MetaData();
-    	currentCells = new byte[rows][columns];
-
-        for (byte[] currentCell : currentCells) {
-            for (int col = 0; col < currentCell.length; col++) {
-                currentCell[col] = 0;
-            }
-        }
+    	currentGeneration = new byte[rows][columns];
+        nextGeneration = new byte[rows][columns];
     }
 
     /**
-     *
-     * @param board
+     * Constructs a boad with the given two-dimensional <code>byte</code>-array and
+     * adds the metadata.
+     * 
+     * @param board The two-dimensional <code>byte</code>-array whose data
+     * are to be placed in the FixedBoard.
+     * @param metadata The <code>MetaData</code> object that contains the metadata
+     * related to the new board.
+     * 
+     * @see MetaData
      */
-    public FixedBoard(byte[][] board) {
-    	metadata = new MetaData();
-    	currentCells = new byte[board.length][board[0].length];
+    public FixedBoard(byte[][] board, MetaData metadata) {
+    	this.metadata = metadata;
+    	currentGeneration = new byte[board.length][board[0].length];
+        nextGeneration = new byte[board.length][board[0].length];
 
-        for(int row = 0; row < board.length; row++) {
-            System.arraycopy(board[row], 0, currentCells[row], 0, board[row].length);
+    	for(int row = 0; row < board.length; row++) {
+            System.arraycopy(board[row], 0, currentGeneration[row], 
+                    0, board[row].length);
     	}
     }
 
-
     /**
-     *
-     * @return
+     * 
+     * @return The number of rows
      */
     public int getRows() {
-        return currentCells.length;
+        return currentGeneration.length;
     }
-
 
     /**
      *
-     * @return
+     * @return The number of columns
      */
     public int getColumns() {
-        return currentCells[0].length;
+        return currentGeneration[0].length;
     }
 
 
     /**
-     *  This method returns meta data from the
+     *  This method returns the meta data from the
      *  board.
      *
      * @return MetaData Meta data from the board contained in
@@ -73,69 +77,123 @@ public class FixedBoard {
     }
 
     /**
-     * This method sets the meta data of the board
-     * @param m
+     * 
+     * @return The board's current generation
      */
-    public void setMetaData(MetaData m) {
-            metadata = m;
-    }
+    public byte[][] getCurrentGeneration() {
+    	byte[][] copy = new byte[currentGeneration.length]
+                [currentGeneration[0].length];
 
-
-
-
-    /**
-     *
-     * @return
-     */
-    public byte[][] getCellArray() {
-    	byte[][] copy = new byte[currentCells.length][currentCells[0].length];
-
-    	for(int row = 0; row < currentCells.length; row++) {
-            System.arraycopy(currentCells[row], 0, copy[row], 0, currentCells[row].length);
+    	for(int row = 0; row < currentGeneration.length; row++) {
+            System.arraycopy(currentGeneration[row], 0, copy[row], 
+                    0, currentGeneration[row].length);
     	}
 
     	return copy;
     }
 
-
     /**
-    * The purpose of this method is to set the setIsAalive value of the entire grid
-    *
-    *
-    * @param board
-    */
-    //Tidligere setBoard-metoden
-    public void setCellArray(byte[][] board) {
-    	for(int row = 0; row < board.length; row++) {
-            System.arraycopy(board[row], 0, currentCells[row], 0, board[row].length);
-    	}
-    }
-
-    /**
-     * Returnerer cellen på gitt posisjon
+     * Returns the <code>byte</code> value of the cell at the given position
      *
      * @param row
      * @param column
-     * @return boolean Returns the boolean value of a cell on the
+     * @return Returns the <code>byte</code> value of a cell on the
      * given position
      */
-    public boolean getCellAliveState(int row, int column) {
-    	byte cell = currentCells[row][column];
-
-        return cell == 1;
+    public byte getCellAliveState(int row, int column) {
+        return currentGeneration[row][column];
     }
 
     /**
+     * Sets the <code>byte</code> value of the cell at the given position
+     * to the <code>byte</code> value given in aliveState. Throws a Runtime
+     * Exception if the number does not equal 0 or 1.
      *
      * @param row
      * @param column
-     * @param isAlive
+     * @param isAlive sets the <code>byte</code> value of a cell on the
+     * given position
      */
-    public void setCellAliveState(int row, int column, boolean isAlive) {
-    	if (isAlive) {
-            currentCells[row][column] = 1;
-        } else {
-            currentCells[row][column] = 0;
+    public void setCellAliveState(int row, int column, byte aliveState) {
+        if(aliveState != 0 || aliveState != 1) {
+            throw new RuntimeException("Invalid number in cell state");
         }
+        currentGeneration[row][column] = aliveState;
+    }
+    
+    /**
+     * Counts the amount of neighbours for the cell at the given position
+     * 
+     * @param row 
+     * @param col
+     * @return the amount of neighbours around the cell
+     */
+    public int countNeighbours(int row, int col) {
+        int neighbours = 0;
+        
+        if(col > 0) {
+            if(currentGeneration[row][col - 1]==1)
+                neighbours++;
+            if( row > 0) {
+                if(currentGeneration[row-1][col - 1]==1)
+                    neighbours++;
+            }
+            if(row < currentGeneration.length - 1) {
+                if(currentGeneration[row+1][col - 1]==1)
+                    neighbours++;
+            }
+        }
+
+        if(col < currentGeneration[0].length - 1) {
+            if(currentGeneration[row][col + 1]==1)
+                neighbours++;
+            if(row > 0) {
+                if(currentGeneration[row-1][col + 1]==1)
+                    neighbours++;
+            }
+            if(row < currentGeneration.length - 1) {
+                if(currentGeneration[row+1][col + 1]==1)
+                    neighbours++;
+            }
+        }
+
+        if(row > 0) {
+            if(currentGeneration[row - 1][col]==1)
+                neighbours++;
+        }
+        if( row < currentGeneration.length - 1) {
+            if(currentGeneration[row + 1][col]==1)
+                neighbours++;
+        }
+        
+        return neighbours;
+    }
+    
+    public void nextGeneration() {
+        for(int row = 0; row < currentGeneration.length; row++) {
+            for(int col = 0; col < currentGeneration[0].length; col++) {
+                nextGeneration[row][col] = ((countNeighbours(row,col) == 3) || 
+                        (currentGeneration[row][col] == 1 && 
+                        countNeighbours(row,col) == 2 )) ? (byte)1 : (byte)0;
+            }
+        }
+        nextGenerationToCurrent();
+    }
+    
+    public void nextGeneration(int startRow, int endRow) {
+        for(int row = 0; row < currentGeneration.length; row++) {
+            for(int col = 0; col < currentGeneration[0].length; col++) {
+                nextGeneration[row][col] = ((countNeighbours(row,col) == 3) || 
+                        (currentGeneration[row][col] == 1 && 
+                        countNeighbours(row,col) == 2 )) ? (byte)1 : (byte)0;
+            }
+        }
+    }
+    
+    public void nextGenerationToCurrent() {
+        for(int row = 0; row < currentGeneration.length; row++) {
+            System.arraycopy(nextGeneration[row], 0, currentGeneration[row], 
+                    0, currentGeneration[row].length);
+    	}
     }
 }
