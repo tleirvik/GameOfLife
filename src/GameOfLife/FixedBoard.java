e390271e63c8361dca16d8c26a0d5f85bf59431e
FullRobotRulle <r.mini.me@gmail.com>
2016-04-01 12:10:50 +0200

    Mer optimalisering av algoritme

--- src/GameOfLife/FixedBoard.java
+++ src/GameOfLife/FixedBoard.java
@@ -10,10 +10,8 @@ package GameOfLife;
  */
 public class FixedBoard {
     private final MetaData metadata;
-    private final byte[][] buffer1;
-    private final byte[][] buffer2;
+    private final byte[][] currentGeneration;
     private final byte[][] firstGeneration;
-    private boolean bufferSwap = false;
     
     /**
      * Constructs a board of a fixed size.
@@ -23,8 +21,7 @@ public class FixedBoard {
      */
     public FixedBoard(int rows, int columns) {
         metadata = new MetaData();
-    	buffer1 = new byte[rows+2][columns+2];
-        buffer2 = new byte[rows+2][columns+2];
+    	currentGeneration = new byte[rows+2][columns+2];
         firstGeneration = new byte[rows+2][columns+2];
     }
 
@@ -41,13 +38,12 @@ public class FixedBoard {
      */
     public FixedBoard(byte[][] board, MetaData metadata) {
     	this.metadata = metadata;
-    	buffer1 = new byte[board.length+2][board[0].length+2];
-        buffer2 = new byte[board.length+2][board[0].length+2];
+    	currentGeneration = new byte[board.length+2][board[0].length+2];
         firstGeneration = new byte[board.length+2][board[0].length+2];
 
     	for(int row = 1; row < board.length-1; row++) {
             for(int col = 1; col < board[0].length-1; col++) {
-                buffer1[row][col] = board[row][col];
+                currentGeneration[row][col] = board[row][col];
                 firstGeneration[row][col] = board[row][col];
             }
     	}
@@ -58,7 +54,7 @@ public class FixedBoard {
      * @return The number of rows
      */
     public int getRows() {
-        return buffer1.length;
+        return currentGeneration.length;
     }
 
     /**
@@ -66,14 +62,7 @@ public class FixedBoard {
      * @return The number of columns
      */
     public int getColumns() {
-        return buffer1[0].length;
-    }
-    
-    /**
-     * Swaps the current buffer that the nextGeneration()-method writes to.
-     */
-    public void swapBuffer() {
-        bufferSwap = !bufferSwap;
+        return currentGeneration[0].length;
     }
 
     /**
@@ -92,20 +81,15 @@ public class FixedBoard {
      * @return The board's current generation
      */
     public byte[][] getBoardReference() {
-    	if(bufferSwap) {
-            return buffer2;
-        } else {
-            return buffer1;
-        }
+    	return currentGeneration;
     }
     
     public void resetBoard() {
         for(int row = 1; row < firstGeneration.length-1; row++) {
             for(int col = 1; col < firstGeneration[0].length-1; col++) {
-                buffer1[row][col] = firstGeneration[row][col];
+                currentGeneration[row][col] = firstGeneration[row][col];
             }
     	}
-        bufferSwap = false;
     }
 
     /**
@@ -117,11 +101,7 @@ public class FixedBoard {
      * given position
      */
     public byte getCellAliveState(int row, int column) {
-        if(bufferSwap) {
-            return buffer1[row][column];
-        } else {
-            return buffer2[row][column];
-        }
+        return currentGeneration[row][column];
     }
 
     /**
@@ -135,14 +115,11 @@ public class FixedBoard {
      * given position
      */
     public void setCellAliveState(int row, int column, byte aliveState) {
-        if(aliveState != 0 || aliveState != 1) {
-            throw new RuntimeException("Invalid number in cell state");
-        } else if(bufferSwap) {
-            buffer2[row][column] = aliveState;
+        if(aliveState == 0 || aliveState == 1) {
+            currentGeneration[row][column] = aliveState;
         } else {
-            buffer2[row][column] = aliveState;
+            throw new RuntimeException("Invalid number in cell state: " + aliveState);
         }
-        
     }
     
     /**
@@ -152,52 +129,48 @@ public class FixedBoard {
      * @param col
      * @return the amount of neighbours around the cell
      */
-    public int countNeighbours(int row, int col, byte[][] currentBuffer) {        
-        return currentBuffer[row-1][col-1] + 
-                currentBuffer[row-1][col] + 
-                currentBuffer[row-1][col+1] + 
-                currentBuffer[row][col-1] + 
-                currentBuffer[row][col+1] + 
-                currentBuffer[row+1][col-1] + 
-                currentBuffer[row+1][col] + 
-                currentBuffer[row+1][col+1];
+    public int countNeighbours(int row, int col) {        
+        return currentGeneration[row-1][col-1] + 
+                currentGeneration[row-1][col] + 
+                currentGeneration[row-1][col+1] + 
+                currentGeneration[row][col-1] + 
+                currentGeneration[row][col+1] + 
+                currentGeneration[row+1][col-1] + 
+                currentGeneration[row+1][col] + 
+                currentGeneration[row+1][col+1];
     }
     
     public void nextGeneration() {
-        byte[][] currentBuffer;
-        byte[][] nextBuffer;
-        if(bufferSwap) {
-            currentBuffer = buffer2;
-            nextBuffer = buffer1;
-        } else {
-            currentBuffer = buffer1;
-            nextBuffer = buffer2;
-        }
-        for(int row = 1; row < currentBuffer.length-1; row++) {
-            for(int col = 1; col < currentBuffer[0].length-1; col++) {
-                nextBuffer[row][col] = ((countNeighbours(row,col,currentBuffer) == 3) || 
-                        (currentBuffer[row][col] == 1 && 
-                        countNeighbours(row,col,currentBuffer) == 2 )) ? (byte)1 : (byte)0;
+        byte[][] neighbourArray = new byte[currentGeneration.length][currentGeneration[0].length];
+        
+        for(int row = 1; row < currentGeneration.length-1; row++) {
+            for(int col = 1; col < currentGeneration[0].length-1; col++) {
+                if(currentGeneration[row][col] == 1) {
+                    neighbourArray[row-1][col-1]++;
+                    neighbourArray[row-1][col]++;
+                    neighbourArray[row-1][col+1]++; 
+                    neighbourArray[row][col-1]++;
+                    neighbourArray[row][col+1]++;
+                    neighbourArray[row+1][col-1]++;
+                    neighbourArray[row+1][col]++;
+                    neighbourArray[row+1][col+1]++;
+                }
             }
         }
-        swapBuffer();
+        
+        for(int row = 1; row < currentGeneration.length-1; row++) {
+            for(int col = 1; col < currentGeneration[0].length-1; col++) {
+                currentGeneration[row][col] = ((neighbourArray[row][col]== 3) || (currentGeneration[row][col] == 1 && neighbourArray[row][col] == 2 )) ? (byte)1 : (byte)0;
+            }
+        }
     }
     
     public void nextGeneration(int startRow, int endRow) {
-        byte[][] currentBuffer;
-        byte[][] nextBuffer;
-        if(bufferSwap) {
-            currentBuffer = buffer2;
-            nextBuffer = buffer1;
-        } else {
-            currentBuffer = buffer1;
-            nextBuffer = buffer2;
-        }
         for(int row = startRow; row < endRow; row++) {
-            for(int col = 0; col < currentBuffer[0].length; col++) {
-                nextBuffer[row][col] = ((countNeighbours(row,col,currentBuffer) == 3) || 
-                        (currentBuffer[row][col] == 1 && 
-                        countNeighbours(row,col,currentBuffer) == 2 )) ? (byte)1 : (byte)0;
+            for(int col = 0; col < currentGeneration[0].length; col++) {
+                currentGeneration[row][col] = ((countNeighbours(row,col) == 3) || 
+                        (currentGeneration[row][col] == 1 && 
+                        countNeighbours(row,col) == 2 )) ? (byte)1 : (byte)0;
             }
         }
     }
