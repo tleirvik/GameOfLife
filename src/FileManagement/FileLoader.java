35eed8727c13325227058dcc9920769edb69cacb
tleirvik <tleirvik>
2016-04-01 09:53:49 +0200

    Merge of RLE to master

--- src/FileManagement/FileLoader.java
+++ src/FileManagement/FileLoader.java
@@ -5,76 +5,91 @@
  */
 package FileManagement;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import GameOfLife.MetaData;
+import GameOfLife.PatternFormatException;
 import util.DialogBoxes;
 
 /**
+ * This class opens a file from disk or a an URL. It passes the file to the RLEDecoder for decoding of the RLE file
  *
  * @author Stian Reistad Røgeberg, Robin Sean Aron David Lundh, Terje Leirvik.
+ * @version 3.0 Created this class to separate file handling from the decoder and encoder
+ * @see RLEDecoder
+ * @see RLEEncoder
  */
 public class FileLoader {
-    private List<String> RLEdata;
-    
-    public FileLoader() {
-        RLEdata = new ArrayList<>();
-    }
-    
-    public boolean readGameBoardFromDisk(File file) {
-        if(file == null) {
+    private RLEDecoder rleDecoder;
+
+    /**
+     *  This method takes a file as input and uses a BufferedReader to read the file. Instantiates a RLEDecoder object
+     *  and calls the decode() method in RLEDecoder for interpreting the file.
+     * @param file File to be read
+     * @return boolean True if the file is parsed by the RLEDecoder
+     * @throws PatternFormatException Throws an exception if the method is unable to parse the RLE file
+     */
+    public boolean readGameBoardFromDisk(File file) throws PatternFormatException {
+        if (file == null) {
             DialogBoxes.infoBox("Error!", "No such file!!", "Please try again!");
             return false;
-        } else if(!file.isFile()) {
-          DialogBoxes.infoBox("Error!", "Invalid file!", "Please try again!");
-           return false;
-        } else if(!file.canRead()) {
-            DialogBoxes.infoBox("Error!", "Could not read file!", 
+        } else if (!file.isFile()) {
+            DialogBoxes.infoBox("Error!", "Invalid file!", "Please try again!");
+            return false;
+        } else if (!file.canRead()) {
+            DialogBoxes.infoBox("Error!", "Could not read file!",
                     "Please try again!");
         }
-        
+
         String line = null;
-        try (BufferedReader reader = 
-                new BufferedReader(new FileReader(file.getAbsolutePath()))) {
-            while ((line = reader.readLine() ) != null) {
-                    RLEdata.add(line);
-            }
-        } catch (FileNotFoundException fnfE) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()))) {
+            rleDecoder = new RLEDecoder(reader);
+            rleDecoder.decode();
+        }  catch (FileNotFoundException fnfE) {
             DialogBoxes.infoBox("Error!", "File was not found", fnfE.getMessage());
+            return false;
+        } catch (PatternFormatException pfE) {
+            DialogBoxes.infoBox("Error!", "The file is not in a compatible format", "The following error occurred trying to interpret game rules: " + pfE.getMessage());
             return false;
         } catch (IOException ioE) {
             DialogBoxes.infoBox("Error!", "An unknown Input/Output error occurred", ioE.getMessage());
             return false;
         }
-        
         return true;
     }
-    
-    public boolean readGameBoardFromURL(String urlString) {
+
+    /**
+     * This method takes a String as input and opens a connection to the specified URL and downloads the file using
+     * InputStreamReader. Instantiates a RLEDecoder object and calls the decode() method in RLEDecoder.
+     * @param urlString String that represents the URL where the file resides
+     * @return boolean True if the file is downloaded and is parsed by the RLEDecoder.
+     * @throws PatternFormatException Throws an exception if the method is unable to parse the RLE file
+     */
+    public boolean readGameBoardFromURL(String urlString) throws PatternFormatException {
         try {
             URL url = new URL(urlString);
             URLConnection conn = url.openConnection();
             try (BufferedReader reader = new BufferedReader(
                     new InputStreamReader(conn.getInputStream()))) {
-                String line = null;
-                while((line = reader.readLine()) != null) {
-                    RLEdata.add(line);
-                }
+                rleDecoder = new RLEDecoder(reader);
+                rleDecoder.decode();
             } catch (IOException ioE) {
                 DialogBoxes.infoBox("Error!", "", "");
                 return false;
             }
-        } catch (MalformedURLException muE) { 
+        } catch (MalformedURLException muE) {
             DialogBoxes.infoBox("Error!", "Invalid URL!", "Please check URL " +
                     "and try again.");
+            return false;
+        } catch (PatternFormatException pfE) {
+            DialogBoxes.infoBox("Error!", "The file is not in a compatible format", "The following error occurred trying to interpret game rules: " + pfE.getMessage());
             return false;
         } catch (IOException ex) {
             DialogBoxes.infoBox("Error!", "", "");
@@ -82,25 +97,24 @@ public class FileLoader {
         }
         return true;
     }
-    
+
     /**
-     * Reads the contents of the input file and stores it in an ArrayList for future use.
+     * Returns the associated MetaData object
      *
-     *This method is not meant to be called directly, but rather through the decode() method in
-     * a RLEDecoder-object.
-     *
-     * @param file
-     * @throws FileNotFoundException
-     * @throws IOException
+     * @return MetaData Returns the boards MetaData(From the RLEDecoder object)
+     * @see RLEDecoder
      */
- 
-    
+    public MetaData getMetadata() {
+        return rleDecoder.getMetadata();
+    }
+
     /**
-     * 
-     * 
-     * @return 
+     *  Returns the associated board(From the RLEDecoder object)
+     *
+     * @return Board Returns a byte[][] board
+     * @see RLEDecoder
      */
-    public List<String> getRLEdata() {
-        return RLEdata;
+    public byte[][] getBoard() {
+        return rleDecoder.getBoard();
     }
 }
--- src/FileManagement/RLEDecoder.java
+++ src/FileManagement/RLEDecoder.java
@@ -1,6 +1,9 @@
 package FileManagement;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import GameOfLife.MetaData;
@@ -10,29 +13,35 @@ import java.util.List;
 import util.DialogBoxes;
 
 /**
+ * This class handles the interpretation of RLE files. One method reads the meta data and creates the game board and
+ * the second method translates the files to our byte[][] game board.
  * 
  * @author Stian Reistad Røgeberg, Robin Sean Aron David Lundh, Terje Leirvik.
+ *
+ * @version 4.0 This class has been the focus of our attention for some time and we developed several versions and
+ * this is as of now our final version. Changed the interpretation logic from regex to if-else branching and we used
+ * JProfiler to measure performance increase upwards of 4000%!
  */
 public class RLEDecoder {
     private MetaData metadata;
     private byte[][] board;
     private List<String> RLEdata;
-
+    private BufferedReader reader;
 
     /**
-     * Constructs a RLEDecoder with file as input
+     * Constructs a RLEDecoder with a BufferedReader as input
      *
-     * @param RLEdata
+     * @param reader BufferedReader file to be parsed
      */
-    public RLEDecoder(List<String> RLEdata) {
-        this.RLEdata = RLEdata;
+    RLEDecoder(BufferedReader reader) {
+        this.reader = reader;
     }
 
     /**
      * "Samlemetode" that controls the flow of the RLE file parsing
      * This method calls other methods in this class with the purpose of
      * doing the following:
-     *
+     * <p>
      * 1. Open a file via FileChooser and read the file to a String
      * 2. Read the x and y value from the String and create a board with those
      * values.
@@ -43,37 +52,33 @@ public class RLEDecoder {
      * dead cells and iterate the board and setting alive cells to
      * true according to the RLE file read)
      *
-     *
      * @return boolean Returns true if the method parses the board with causing an
      * Exception
      * @throws PatternFormatException Throws an exception if the method is unable to
-     * parse the RLE file
+     *                                parse the RLE file
      */
-    public boolean decode() {
-        getMetaData();
-
+    public boolean decode() throws PatternFormatException, IOException {
         try {
-            getBoardSize();
-        } catch (PatternFormatException pfE) {
-            DialogBoxes.infoBox("Error!", "The file is not in a compatible format", "The following error occurred trying to interpret board size: " + pfE.getMessage());
-            return false;
-        }
-
-        try {
-            getGameRuleString();
-        } catch (PatternFormatException pfE) {
-            DialogBoxes.infoBox("Error!", "The file is not in a compatible format", "The following error occurred trying to interpret game rules: " + pfE.getMessage());
-            return false;
-        }
-
-        try {
-            parseBoard();
+            parseMetadata(reader);
         } catch (PatternFormatException pfE) {
             DialogBoxes.infoBox("Error!", "The file is not in a compatible format", "The following error occurred trying to interpret board content " + pfE.getMessage());
             return false;
         }
-
+        try {
+            parseBoard(reader);
+        } catch (PatternFormatException pfE) {
+            DialogBoxes.infoBox("Error!", "The file is not in a compatible format", "The following error occurred trying to interpret board content " + pfE.getMessage());
+            return false;
+        }
         return true;
+    }
+    private void removeComments() {
+        for(int i = 0; i < RLEdata.size(); i++) {
+            if(RLEdata.get(i).startsWith("#")) {
+                RLEdata.remove(i);
+                //i--;
+            }
+        }
     }
 
     /**
@@ -83,224 +88,150 @@ public class RLEDecoder {
      * This method is not meant to be called directly, but rather through the decode() method in
      * a RLEDecoder-object.
      *
-     * @throws PatternFormatException
-     * @see MetaData.java
+     * @throws PatternFormatException Throws an exception if the method is unable to parse the RLE file
+     * @see MetaData
      */
-    private void getMetaData() {
+    private void parseMetadata(BufferedReader reader) throws PatternFormatException, IOException {
         metadata = new MetaData();
         StringBuilder name = new StringBuilder();
         StringBuilder author = new StringBuilder();
         StringBuilder comment = new StringBuilder();
-
-        for (int i = 0; i < RLEdata.size(); i++) {
-            String line = RLEdata.get(i);
-
-            if (line.contains("#N")) {
-                    String tempString = line.replaceAll("(#N )", "");
-                    name.append(tempString);
-            } else if (line.contains("#C")) {
-                    String tempString = line.replaceAll("(#C )", "");
-                    comment.append(tempString);
-                    comment.append("\n");
-            } else if (line.contains("#c")) {
-                    String tempString = line.replaceAll("(#c )", "");
-                    comment.append(tempString);
-                    comment.append("\n");
-            } else if (line.contains("#O")) {
-                    String tempString = line.replaceAll("(#O )", "");
-                    author.append(tempString);
-            }
-        }
-        metadata.setName(name.toString());
-        metadata.setAuthor(author.toString());
-        metadata.setComment(comment.toString());
-
-        // This for-loop needs to decrement i to account for the
-        // changes in RLEdata when removing an element (subtracts
-        // one from the indices).
-        for(int i = 0; i < RLEdata.size(); i++) {
-            if(RLEdata.get(i).startsWith("#")) {
-                RLEdata.remove(i);
-                i--;
-            }
-        }
-    }
-
-    /**
-     * Reads the board's X and Y size from the previously loaded contents and creates a two-dimensional boolean grid
-     *
-     * This method is not meant to be called directly, but rather through the decode() method in
-     * a RLEDecoder-object.
-     *
-     * @throws PatternFormatException
-     * @return void
-     *
-     */
-    private void getBoardSize() throws PatternFormatException {
         Pattern RLEpatternY = Pattern.compile("([yY][\\s][=][\\s])([\\d]+)");
         Pattern RLEpatternX = Pattern.compile("([xX][\\s][=][\\s])([\\d]+)");
 
-        boolean foundRows = false;
-        boolean foundColumns = false;
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            if (line.charAt(0) == '#') {
+                final char tempChar = line.charAt(1);
+                switch (tempChar) {
+                    case 'N':
+                        String tempString = line.replaceAll("(#N )", "");
+                        name.append(tempString);
+                        metadata.setName(name.toString());
+                        System.out.println(name.toString());
+                        break;
+                    case 'C':
+                        String tempString2 = line.replaceAll("(#C )", "");
+                        comment.append(tempString2);
+                        comment.append("\n");
+                        metadata.setComment(comment.toString());
+                        break;
+                    case 'c':
+                        String tempString3 = line.replaceAll("(#c )", "");
+                        comment.append(tempString3);
+                        comment.append("\n");
+                        metadata.setComment(comment.toString());
+                        break;
+                    case 'O':
+                        String tempString4 = line.replaceAll("(#O )", "");
+                        author.append(tempString4);
+                        metadata.setAuthor(author.toString());
+                }
 
-        int rows = 0;
-        int columns = 0;
+            } else {
+                boolean foundRows = false;
+                boolean foundColumns = false;
+                Matcher RLEmatcherY = RLEpatternY.matcher(line);
+                Matcher RLEmatcherX = RLEpatternX.matcher(line);
+                int rows = 0;
+                int columns = 0;
 
-        for (int i = 0; i < RLEdata.size(); i++) {
-            String line = RLEdata.get(i);
-            Matcher RLEmatcherY = RLEpatternY.matcher(line);
-            Matcher RLEmatcherX = RLEpatternX.matcher(line);
-
-            if(RLEmatcherY.find()) {
+                if (RLEmatcherY.find()) {
                     rows = Integer.parseInt(RLEmatcherY.group(2));
                     foundRows = true;
-            }
+                }
 
-            if(RLEmatcherX.find()) {
+                if (RLEmatcherX.find()) {
                     columns = Integer.parseInt(RLEmatcherX.group(2));
                     foundColumns = true;
-            }
-
-            if(foundRows && foundColumns) {
-                board = new byte[rows][columns];
-                return;
-            }
-        }
-
-        if(!foundRows && !foundColumns) {
-            throw new PatternFormatException("X and Y values could not be parsed from RLE-file");
-        } else if(!foundRows) {
-            throw new PatternFormatException("Y values could not be parsed from RLE-file");
-        } else if(!foundColumns) {
-            throw new PatternFormatException("X values could not be parsed from RLE-file");
-        }
-    }
-
-    /**
-     * Reads the game's rulestring from the previously loaded contents and stores it in a MetaData-object.
-     *
-     * This method is not meant to be called directly, but rather through the decode() method in
-     * a RLEDecoder-object.
-     *
-     * @throws PatternFormatException
-     * @return void
-     */
-    private void getGameRuleString() throws PatternFormatException {
-        Pattern RLEpatternRules =
-                Pattern.compile("rule[\\s]=[\\s][bB]([\\d]+)/[sS]([\\d]+)");
-        String[] SBrules = new String[2];
-        boolean foundRules = false;
-
-        for (int i = 0; i < RLEdata.size(); i++) {
-            String line = RLEdata.get(i);
-            Matcher RLEmatcherRules = RLEpatternRules.matcher(line);
-
-            if (RLEmatcherRules.find()) {
-                SBrules[0] = RLEmatcherRules.group(1);
-                SBrules[1] = RLEmatcherRules.group(2);
-                foundRules = true;
-                RLEdata.remove(i);
-                metadata.setRuleString(SBrules);
-                return;
-            }
-        }
-
-        RLEpatternRules =
-                Pattern.compile("rule[\\s]=[\\s]([\\d]+)/([\\d]+)");
-        for(int i = 0; i < RLEdata.size(); i++) {
-            String line = RLEdata.get(i);
-            Matcher RLEmatcherRules = RLEpatternRules.matcher(line);
-
-            if(RLEmatcherRules.find()) {
-                SBrules[0] = RLEmatcherRules.group(1);
-                SBrules[1] = RLEmatcherRules.group(2);
-                foundRules = true;
-                RLEdata.remove(i);
-                metadata.setRuleString(SBrules);
-                return;
-            }
-        }
-        
-        RLEpatternRules =
-                Pattern.compile("rule[\\s]=[\\s][sS]([\\d]+)/[bB]([\\d]+)");
-        for (int i = 0; i < RLEdata.size(); i++) {
-            String line = RLEdata.get(i);
-            Matcher RLEmatcherRules = RLEpatternRules.matcher(line);
-
-            if (RLEmatcherRules.find()) {
-                SBrules[0] = RLEmatcherRules.group(2);
-                SBrules[1] = RLEmatcherRules.group(1);
-                foundRules = true;
-                RLEdata.remove(i);
-                metadata.setRuleString(SBrules);
-                return;
-            }
-        }
-        
-        if(!foundRules) {
-            throw new PatternFormatException("Game rules could not be parsed from RLE-file");
-        }
-    }
-
-
-    /**
-     * This method parses the board cells from the previously
-     * loaded contents.
-     *
-     * This method is not meant to be called directly, but rather through the decode() method in
-     * a RLEDecoder-object.
-     *
-     * @throws PatternFormatException
-     * @return void
-     */
-    public void parseBoard() throws PatternFormatException{
-        // Pattern RLEpattern = Pattern.compile("([0-9]+(?=[bBoO]))|([bBoO])");
-    	Pattern RLEpattern = Pattern.compile("([0-9]+[bBoO])|([bBoO])");
-        String tempString = "";
-
-        for (String s : RLEdata) {
-        	tempString += s;
-        }
-
-        String[] tempRLEArray = tempString.split("\\$");
-
-        for(int row = 0; row < tempRLEArray.length; row++) {
-            Matcher RLEMatcher = RLEpattern.matcher(tempRLEArray[row]);
-            int column = 0;
-
-            while (RLEMatcher.find()) {
-            	if (RLEMatcher.group(2) != null) {
-                	if (RLEMatcher.group(2).matches("[oO]")) {
-                		board[row][column] = 1;
-    	        	}
-                	column++;
-                } else if (RLEMatcher.group(1) != null) {
-                	Pattern p = Pattern.compile("([0-9]+)([oObB])");
-                	Matcher m = p.matcher(RLEMatcher.group(1));
-
-                	m.find();
-                	int number = Integer.parseInt(m.group(1));
-
-                	while (number-- != 0) {
-		            	if(m.group(2).matches("[oO]")) {
-		            		board[row][column] = 1;
-		            	}
-		            	column++;
-		            }
                 }
-            } // End of first while-loop
-        } // End of for-loop
-    } // End of method parseBoard()
+
+                if (foundRows && foundColumns) {
+                    board = new byte[rows][columns];
+                    // System.out.println("Board created :" + board + " x: " + columns + " y: " + rows);
+                    return;
+                }
+
+                if (!foundRows && !foundColumns) {
+                    throw new PatternFormatException("X and Y values could not be parsed from RLE-file");
+                } else if (!foundRows) {
+                    throw new PatternFormatException("Y values could not be parsed from RLE-file");
+                } else if (!foundColumns) {
+                    throw new PatternFormatException("X values could not be parsed from RLE-file");
+                }
+            }
+        }
+    }
+
+    /**
+     * This method parses the RLE file and converts the contents to a byte[][] board.
+     *
+     * @param reader BufferedReader The file to be interpreted
+     * @throws PatternFormatException Throws an exception if the method is unable to parse the RLE file
+     * @throws IOException Other unspecified I/O related issues
+     *
+     * @version 3.0 Complete changed the logic. Converted from regex to if-else and
+     * we observed a > 3800% performance gain. Large RLE files now usually never takes more than a few hundred
+     * milisecounds compared to >40 seconds using regex
+     *
+     */
+    private void parseBoard(BufferedReader reader) throws PatternFormatException, IOException {
+        String line = null;
+        int row = 0, col = 0;
+        final char lineBreak = '$';
+
+        while ((line = reader.readLine()) != null) {
+            int charNumber = 0; // Antall forekomster av en celle
+            for (int j = 0; j < line.length(); j++) {
+                if (Character.isDigit(line.charAt(j))) {
+                    if (charNumber == 0) {
+                        charNumber = Character.getNumericValue(line.charAt(j));
+                    } else {
+                        charNumber *= 10;
+                        charNumber += Character.getNumericValue(line.charAt(j));
+                    }
+                } else if (line.charAt(j) == 'o' || line.charAt(j) == 'O') {
+                    if (charNumber != 0) { //Iterer gjennom antall forekomster og sett celle til levende
+                        while (charNumber != 0) {
+                            board[row][col] = 1;
+                            charNumber--;
+                            col++;
+                        }
+                    } else {
+                        board[row][col] = 1;
+                        col++;
+                    }
+                } else if (line.charAt(j) == 'b' || line.charAt(j) == 'B') {
+                    if (charNumber != 0) {
+                        while (charNumber != 0) {
+                            charNumber--;
+                            col++;
+                        }
+                    } else {
+                        col++;
+                    }
+                } else if (line.charAt(j) == lineBreak) {
+                    row++;
+                    col = 0;
+                    charNumber = 0;
+                }
+            }
+        }
+    }
 
     /**
      * Method that returns the board contained in this class
-     * @return board Returns the boolean[][] board
-     * contained in this class
+     * @return board Returns the boolean[][] board contained in this class
      */
     public byte[][] getBoard() {
             return this.board;
     }
 
+    /**
+     * Method that returns the associated MetaData object that we read from the RLE files
+     *
+     * @return metadata A MetaData object that contains relevant metadata about the board
+     */
     public MetaData getMetadata() {
         return this.metadata;
     }
