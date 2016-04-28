6fd90070b58561d942b81c80b899d10e845ba937
stianreistadrogeberg <stianpiano@protonmail.com>
2016-04-28 09:45:48 +0200

    import

--- src/Model/FileManagement/OtherFormats/GIFSaver.java
+++ src/Model/FileManagement/OtherFormats/GIFSaver.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model.FileManagement;
+package Model.FileManagement.OtherFormats;
 
 import Model.GameOfLife.GameOfLife;
 import lieng.GIFWriter;
