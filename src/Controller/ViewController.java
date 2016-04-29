b7cbe3acf87e227b0ef6bb92404f10806bdeadb7
Terje Leirvik <Terje Leirvik>
2016-04-29 16:52:57 +0200

    Dynamic Board changes in View Controller

--- src/Controller/ViewController.java
+++ src/Controller/ViewController.java
@@ -1,11 +1,12 @@
 package Controller;
 
-import Model.FileManagement.FileLoader;
 import Model.FileManagement.Decoders.RLEDecoder;
 import Model.FileManagement.Encoders.RLEEncoder;
-import Model.FileManagement.OtherFormats.WavSaver;
+import Model.FileManagement.FileLoader;
 import Model.GameOfLife.Boards.Board.BoardType;
 import Model.GameOfLife.GameOfLife;
+import Model.util.DialogBoxes;
+import Model.util.Stopwatch;
 import Wav.WavFile;
 import javafx.animation.Animation;
 import javafx.animation.Animation.Status;
@@ -25,16 +26,10 @@ import javafx.scene.paint.Color;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import Model.util.DialogBoxes;
-import Model.util.Stopwatch;
-import Wav.WavFileException;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *Denne klassen lytter på hendelser i .fxml
@@ -132,6 +127,7 @@ public class ViewController {
 
     @FXML
     public void openAbout() {
+       /*
         try {
             new WavSaver(gol, 20, null, 0, 0, 0, 0);
         } catch (IOException ex) {
@@ -139,6 +135,7 @@ public class ViewController {
         } catch (WavFileException ex) {
             Logger.getLogger(ViewController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        */
     }
     
     @FXML
@@ -576,56 +573,20 @@ public class ViewController {
 
     private void initializeMouseEventHandlers() {
         gameCanvas.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent e) -> {
-            if(drawMode) {
-                //TEGNEMODUS
+            if(drawMode) { //TEGNEMODUS
                 double bClick_X = e.getX();
                 double bClick_Y = e.getY();
-
-                if(isXInsideGrid(bClick_X) && isYInsideGrid(bClick_Y)) {
-                    int row = (int) ((bClick_Y - (getGridStartPosY() - getBoardHeight())) / cellSize) - gol.getRows();
-                    int column = (int) ((bClick_X - (getGridStartPosX() - getBoardWidth())) / cellSize) - gol.getColumns();
-                    if ((row < gol.getRows()) && (row >= 0) && (column < gol.getColumns() ) && (column >= 0)) {
-                        System.out.println("row " + row + " col " + column);
-                        if(holdingPattern) {
-                            //drawObject(row, column, pattern)
-                            byte[][] testArray = new byte[][] {
-                                        {0, 1, 0},
-                                        {0, 0, 1},
-                                        {1, 1, 1}
-                            };
-
-                            final int M = testArray.length;
-                            final int N = testArray[0].length;
-                            byte[][] ret = new byte[N][M];
-                            for(int r = 0; r < M; r++) {
-                                for (int c = 0; c < N; c++) {
-                                    ret[c][M-1-r] = testArray[r][c];
-                                }
-                            }
-
-                            int mid = (0 + ret.length - 1) / 2;
-
-                            for(int i = 0; i < ret.length; i++) {
-                                for(int j = 0; j < ret[i].length; j++) {
-                                    gol.setCellAliveState(row + i -mid, column + j -mid, ret[i][j]);
-                                }
-                            }
-                            holdingPattern = false;
-                        } else {
-                            drawCell = (gol.getCellAliveState(row, column) != 1);
-                            gol.setCellAliveState(row, column, (drawCell ? (byte)1 : (byte)0));
-                        }
-                        draw();
-                    }
-                }
-
+                int row = (int) ((bClick_Y - (getGridStartPosY() - getBoardHeight())) / cellSize) - gol.getRows();
+                int column = (int) ((bClick_X - (getGridStartPosX() - getBoardWidth())) / cellSize) - gol.getColumns();
+                drawCell = (gol.getCellAliveState(row, column) != 1);
+                gol.setCellAliveState(row, column, (drawCell ? (byte)1 : (byte)0));
+                draw();
             } else {//FLYTTEFUNKSJON
                 fitToView.setSelected(false);
                 offsetBegin_X = e.getX() - getGridStartPosX();
                 offsetBegin_Y = e.getY() - getGridStartPosY();
             } // end if
-        } // end handle
-        ); // end eventhandler
+        }); // end eventhandler
 
         gameCanvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, (MouseEvent e) -> {
             if(drawMode) {
