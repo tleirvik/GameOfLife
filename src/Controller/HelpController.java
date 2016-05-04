f83782baca8450b1dc23755e03b7c187c40c9029
FullRobotRulle <r.mini.me@gmail.com>
2016-05-04 18:15:18 +0200

    Ferdigstilt innlasting av HTML filer i hjelpsystemet

--- src/Controller/HelpController.java
+++ src/Controller/HelpController.java
@@ -5,6 +5,10 @@
  */
 package Controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.web.WebEngine;
@@ -22,13 +26,53 @@ public class HelpController {
     @FXML private ListView listView;
     @FXML private WebView webView;
     
+    private WebEngine webEngine;
+    
     public void initializeHelp() {
-        
-        listView.get
-        
-        WebEngine webEngine = webView.getEngine();
-        webEngine.load("http://google.com");
-        
-        
+        webEngine = webView.getEngine();
+        initializeListView();
     }
+    
+    private void initializeListView() {
+        /*
+        About/Credits
+        How to Play
+        Pattern Editor
+        Statistics
+        ...
+        */
+        ObservableList<String> subjects = FXCollections.observableArrayList(
+                "About / Credits","How To Play", "Pattern Editor", "Statistics"
+        );
+        listView.setItems(subjects);
+        
+        listView.getSelectionModel().selectedItemProperty().addListener(
+            new ChangeListener<String>() {
+                public void changed(ObservableValue<? extends String> ov, 
+                    String old_val, String new_val) {
+                    webEngine.load(getSubjectURL(new_val));
+            }
+        });
+        listView.getSelectionModel().select(0);
+    }
+    
+    private String getSubjectURL(String subject) {
+        String subjectFileName = "AboutCredits.html";
+        switch(subject) {
+            case "About / Credits":
+                subjectFileName = "AboutCredits.html";
+                break;
+            case "How To Play":
+                subjectFileName = "HowToPlay.html";
+                break;
+            case "Pattern Editor":
+                subjectFileName = "PatternEditor.html";
+                break;
+            case "Statistics":
+                subjectFileName = "Statistics.html";
+                break;
+        }
+        return getClass().getResource("/Model/util/help/" + subjectFileName).toExternalForm();
+    }
+    
 }
