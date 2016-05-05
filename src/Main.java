3d006789571d0a2b71dcd3474e731fd6b686f0fc
tleirvik <terje.leirvik@gmail.com>
2016-05-05 16:37:26 +0200

    Refaktorering / Code cleanup

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,26 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	@Override
-	public void start(Stage stage) throws Exception {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainWindow.fxml"));
-            Parent root = (Parent)loader.load();
-            ViewController controller = (ViewController)loader.getController();
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainWindow.fxml"));
+        Parent root = (Parent) loader.load();
+        ViewController controller = (ViewController) loader.getController();
 
-            Scene scene = new Scene(root);
+        Scene scene = new Scene(root);
 
-            
-            scene.getStylesheets().add(getClass().getResource(
-                    "/view/mainWindowStyleSheet.css").toExternalForm());
-                
-            stage.setTitle("Game Of Life");
-            stage.setScene(scene);
-            stage.setMinHeight(690);
-            stage.setMinWidth(910);
-            stage.show();
-	}
+
+        scene.getStylesheets().add(getClass().getResource(
+                "/view/mainWindowStyleSheet.css").toExternalForm());
+
+        stage.setTitle("Game Of Life");
+        stage.setScene(scene);
+        stage.setMinHeight(690);
+        stage.setMinWidth(910);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-    	launch(args);
+        launch(args);
     }
 }
